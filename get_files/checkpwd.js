194d40545f45f5b3c241ec9ca14b3295e2a055c0
gt-league <137290723+gt-league@users.noreply.github.com>
2023-07-27 10:33:43 +0800

    Add files via upload

---
+++ get_files/checkpwd.js
@@ -0,0 +1,31 @@
+﻿function checkPass(s) {
+    if (s.length < 6) {
+
+        return 0;
+    }
+    var ls = 0;
+
+    if (s.match(/([a-z])+/)) {
+
+        ls++;
+
+    }
+
+    if (s.match(/([0-9])+/)) {
+
+        ls++;
+    }
+
+    if (s.match(/([A-Z])+/)) {
+
+        ls++;
+
+    }
+    //if (s.match(/[^a-zA-Z0-9]+/)) {
+
+    //   ls++;
+
+   // }
+    return ls
+
+} 
